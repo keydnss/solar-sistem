f23d6eb6b624174a3660daea09440b702b38f6db
Ricardo Albuquerque <162588790+keydnss@users.noreply.github.com>
2025-02-18 13:30:26 -0300

    Add files via upload

---
+++ sistema-solar/script.js
@@ -0,0 +1,9 @@
+function mostrarInfo(id) {
+    let info = document.getElementById(id);
+    info.style.display = "flex";
+}
+
+function fecharInfo(id) {
+    let info = document.getElementById(id);
+    info.style.display = "none";
+}
